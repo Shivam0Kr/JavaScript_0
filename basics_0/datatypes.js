4748a804c255ecbc9e2e05c1fa046edbd608ef12
Shivam0Kr <146299171+Shivam0Kr@users.noreply.github.com>
2023-10-12 11:11:20 +0000

    datatypes

---
+++ basics_0/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"    // treats all JS code as a newer version of code and implementation
+
+//alert("hello")  // we can only use this in web browser document, console not in node js
+
+/* useful websites
+tc39.es
+mdn web docs
+*/
+
+// different datatypes
+
+// null - standlone value
+// Number
+// Boolean
+// BigInt
+// String
+// symbol- mostly used in React for uniques
+
+console.log(typeof "Shivam");
+console.log(typeof null);
+console.log(typeof undefined);
+
+console.log("Shivam Kumar");
