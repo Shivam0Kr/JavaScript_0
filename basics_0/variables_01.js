3be2ab9ecffb4c2d7cf8be84c2e540e17f86bdad
Shivam0Kr <146299171+Shivam0Kr@users.noreply.github.com>
2023-10-12 10:48:21 +0000

    update

---
+++ basics_0/variables_01.js
@@ -0,0 +1,11 @@
+const id = 2467890
+let name = "Shivam"
+let email = "123@gmail.com"
+var accPass = "3435687"
+var city = "Jaipur"
+let state               // if value is undeclared, it is undefined
+console.log(id)
+
+console.table([id, name, email, accPass, city, state])
+
+// prefer not to use var because of block and functional scope
