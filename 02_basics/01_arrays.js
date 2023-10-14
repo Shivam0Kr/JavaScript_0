c6c9982b9da3f286bd87639ec5212f859c734f70
Shivam0Kr <146299171+Shivam0Kr@users.noreply.github.com>
2023-10-14 09:39:13 +0000

    array part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -20,4 +20,23 @@ const arr2 = new Array(1,2,3,4)
 // arr2.shift()
 // console.log(arr2);
 
-console.log(arr2.includes(9));
+// console.log(arr2.includes(9));          //false  
+
+// console.log(arr2.indexOf(9));              // if value doesn't exist it gives output as -1
+// console.log(arr2.indexOf(3));
+
+const newArr = arr2.join()              // join convert the type to string(newArr) and combine array 
+console.log(arr2);
+console.log(newArr);
+
+console.log("------------- slice,    splice -------------");
+
+console.log("A ", arr2);
+
+const myArr1 = arr2.slice(1,3)
+console.log("B ", arr2);
+console.log(myArr1);
+
+const myArr2 = arr2.splice(1,3)
+console.log("C ", arr2);
+console.log(myArr2);
