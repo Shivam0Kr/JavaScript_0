cedf5e89659672cda1f8155650bd41846efd3066
Shivam0Kr <146299171+Shivam0Kr@users.noreply.github.com>
2023-11-07 11:08:04 +0000

    complete

--- 04_control_flow/first.js
+++ 04_control_flow/first.js
@@ -1,38 +1,112 @@
-const userLoggedIn = false;
-if(userLoggedIn){
-    console.log("true");
-}
-else
-{
-    console.log("negative");
+// const userLoggedIn = false;
+// if(userLoggedIn){
+//     console.log("true");
+// }
+// else
+// {
+//     console.log("negative");
+// }
+
+// /////////////////////////////
+
+// let power = "4000"
+// const score = 200;
+// if(score > 100){
+//     const power = "fly"
+//     console.log("User power is "+ power);
+//     console.log(`User power is: ${power}`);
+// }
+// console.log(`User power is: ${power}`);
+
+// ///////////////////////////////
+// const balance = 1000
+// if (balance < 2000) console.log("low balance");             // implicit scope       not usually recommended
+// //////////////////////////////
+
+// const totalBalance = 2000
+// if (totalBalance>1000){
+//     console.log("greater than 1000");
+// }
+// else if(totalBalance==1000){
+//     console.log("not equal to 1000");
+// }
+// else{
+//     console.log(`your balance is: ${totalBalance}`);
+// }
+// console.log(`your balance is: ${totalBalance}`);
+
+
+
+const loggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = true;
+const loggedInFromEmail = false;
+
+if(loggedIn== true && debitCard== true){
+    console.log("Allow the user to do action");
 }
 
-/////////////////////////////
-
-let power = "4000"
-const score = 200;
-if(score > 100){
-    const power = "fly"
-    console.log("User power is "+ power);
-    console.log(`User power is: ${power}`);
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("user loggedIn");
 }
-console.log(`User power is: ${power}`);
 
-///////////////////////////////
-const balance = 1000
-if (balance < 2000) console.log("low balance");             // implicit scope       not usually recommended
-//////////////////////////////
+// -----------------------  Switch ------------------------------
 
-const totalBalance = 2000
-if (totalBalance>1000){
-    console.log("greater than 1000");
+const month = 2;
+switch (month) {
+    case 1:
+        console.log("Jam")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("Mar")
+        break;
+    case 4:
+        console.log("Apr")
+        break;
+
+    default:
+        break;
 }
-else if(totalBalance==1000){
-    console.log("not equal to 1000");
+
+// ---------------------------  thruthy and falsy  -------------------
+
+// falsy values -> false, 0, -0, BigInt 0n, "", null, undefined, NaN   other than this are thruthy values
+// truthy values    ->  "0", "false", "  ", [], {}, function(){}
+
+const userEmail = []
+if(userEmail){
+    console.log("Got user email");
 }
 else{
-    console.log(`your balance is: ${totalBalance}`);
+    console.log("user can't login");
 }
-console.log(`your balance is: ${totalBalance}`);
+
+// ---------------------
+if(userEmail.length == 0){
+    console.log("Array is empty");
+}
+
+// ---------------------
+const  emptyObj= {}
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+if(false == 0){                     //  false == "", 0 == ' '  
+    console.log(true);
+}
 
 
+// ---------------------------------------  Nullish Coalescing Operator ??  -------------------------
+// let val = 5 ?? 10
+// let val =  null ?? 10
+// let val = 5 ?? undefined
+let val =  null ?? 10 ?? 15
+
+console.log(val);
+
+// ------------------------------  Terniary Operator  ------------------
+// condition ? true : false
